0014f00730786f4a371c26f35c4040fe7dd2f327
Tyuiu.GrigorjanAM@outlook.com <djuravlik323@gmail.com>
2023-12-12 12:07:12 +0500

    Add project files.

---
+++ Tyuiu.GrigorjanAM.Sprint7/FormMain.Designer.cs
@@ -0,0 +1,71 @@
+﻿
+namespace Tyuiu.GrigorjanAM.Sprint7
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelButtons_GAM = new System.Windows.Forms.Panel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // panelButtons_GAM
+            // 
+            this.panelButtons_GAM.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelButtons_GAM.Location = new System.Drawing.Point(0, 0);
+            this.panelButtons_GAM.Name = "panelButtons_GAM";
+            this.panelButtons_GAM.Size = new System.Drawing.Size(884, 87);
+            this.panelButtons_GAM.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 87);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 374);
+            this.panel1.TabIndex = 1;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 461);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panelButtons_GAM);
+            this.Name = "FormMain";
+            this.Text = "Каталог видео-клипов";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelButtons_GAM;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
